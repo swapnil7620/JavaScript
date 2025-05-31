60d2015f9305304df6660cbe599bd42c6a100e04
swapnil <130269745+swapnil7620@users.noreply.github.com>
2025-05-31 19:27:07 +0530

    Loops

---
+++ Loops/Loops.js
@@ -0,0 +1,70 @@
+
+
+// Print the Number from 1 to 10
+/*
+for(let i =0; i<=10;i++ ){
+    console.log("i : " + (i));
+}
+    */
+
+// Print the Number from 100 to 200
+/*
+for(let i =100; i<=200;i++ ){
+    console.log("i : " + (i));
+}
+    */
+
+
+// Print the Number from 100 to 1
+
+/*
+for(let i =100; i>=1;i-- ){
+    console.log("i : " + (i));
+}
+    */
+
+// Print the even number from 50 to 10
+/*
+for(let i =50; i>=10;-i--){
+    if(i % 2 ==0){
+        console.log(i);
+}
+}
+  */
+// print the addition of number from 10 to 100
+/*
+let Sum =0;
+for(let i =10; i<=100;i++){
+         Sum += i;
+}
+console.log(Sum);
+*/
+
+// print the addition of number from 59 to 100
+
+/*
+let Sum =0;
+for(let i =59; i<=100;i++){
+         Sum += i;
+}
+console.log(Sum);
+*/
+
+// Print All the even number from 1 to 20 using while loop
+
+/*
+let i = 0;
+while (i <= 20) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+    i++;
+}
+*/
+
+// print the table of 5 
+let i = 1;
+while (i<=10) {
+    console.log( i + " * 5  = " + (i * 5));
+    i++;
+}
---
+++ Loops/Script.js
@@ -0,0 +1,51 @@
+console.log("Hello");
+
+//let i=0
+// for ( i; i <= 10; i++) {
+    
+//       console.log(i);
+    
+// }
+
+// i=0;
+// while (i<10) {
+//    console.log(i);
+//    i++;
+      
+// }
+
+
+// do {
+//       console.log("hello")
+// } while (i<10){
+//       console.log(i);
+//       i++;
+// }
+
+// use for array
+
+// const fruits=["A", "B","C"];
+// for(let f of fruits){
+//       console.log(fruits);
+// } 
+
+
+// use for Object
+
+// const person ={Name :"John",age:"24"};
+// for(let key in person){
+//       console.log(key,person[key]);
+// }
+
+// for(i=1;i<=10;i++){
+//       if(i==3){
+//             // continue;
+//             break;
+//       }
+//       else{
+//             console.log(i);
+//       }
+
+// }
+
+
